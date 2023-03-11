a911836d256924b02e6938ebb149ee8bdf031555
Dan Vega <danvega@gmail.com>
2023-03-11 15:37:02 -0500

    Download cover on build because it expires

--- server/api/notion/database-query.get.js
+++ server/api/notion/database-query.get.js
@@ -1,4 +1,8 @@
 import { Client } from '@notionhq/client'
+import fs from 'fs';
+import Axios from 'axios';
+
+
 
 export default defineEventHandler(async (event) => {
 
@@ -20,15 +24,36 @@ export default defineEventHandler(async (event) => {
   });
   const posts = [];
   database.results.forEach((post) => {
+
+    // cover: post.properties.Image.files[0]?.file.url,
+    const imgName = post.properties.Image.files[0]?.name;
+
     posts.push({
       id: post.id,
       title: post.properties.Name.title[0].plain_text,
-      cover: post.properties.Image.files[0]?.file.url,
       slug: post.properties.Slug.rich_text[0].plain_text,
       excerpt: post.properties.Excerpt.rich_text[0].plain_text,
+      cover: post.properties.Image.files[0]?.name,
     });
+    // the cover images expire
+    downloadImage(post.properties.Image.files[0]?.file.url, `./public/images/${imgName}`);
   });
+
+  async function downloadImage(url, filepath) {
+    const response = await Axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    });
+    return new Promise((resolve, reject) => {
+        response.data.pipe(fs.createWriteStream(filepath))
+            .on('error', reject)
+            .once('close', () => resolve(filepath));
+    });
+  }
 
   return posts;
 
-})
+});
+
+
